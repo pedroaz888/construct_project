b3826f1ef4b9e506d1ca28f7660cef6775449070
PedroAz <pdegutty@gmail.com>
2023-04-26 10:03:05 -0300

    db

--- construct_youtube/settings.py
+++ construct_youtube/settings.py
@@ -87,16 +87,15 @@ WSGI_APPLICATION = 'construct_youtube.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-
-
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'railway',
-            'USER': 'root',
-            'PASSWORD': 'U3gXhNLDnwKITofoWitW',
-            'HOST':'containers-us-west-142.railway.app',
-            'PORT':'6634',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qIdTHDne7cjA5rwqvD6K',
+        'HOST': 'containers-us-west-155.railway.app',
+        'PORT': '7318',
+
     }
 }
 
